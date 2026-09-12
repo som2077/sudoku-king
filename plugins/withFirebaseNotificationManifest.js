ea6c55556a07a8721eb5c714b410ff895898d42b
som2077 <somgautam2077@gmail.com>
2026-09-12 16:02:59 +0530

    feat: add default notification icon handling in Firebase manifest

--- plugins/withFirebaseNotificationManifest.js
+++ plugins/withFirebaseNotificationManifest.js
@@ -2,10 +2,12 @@ const { withAndroidManifest } = require('expo/config-plugins');
 
 const FIREBASE_NOTIFICATION_COLOR =
   'com.google.firebase.messaging.default_notification_color';
+const FIREBASE_NOTIFICATION_ICON =
+  'com.google.firebase.messaging.default_notification_icon';
 
 /**
- * Firebase Messaging declares a default notification color. Expo Notifications
- * supplies this app's configured color, so it must win during manifest merge.
+ * Firebase Messaging declares a default notification color and icon. Expo Notifications
+ * supplies this app's configured color and icon, so they must win during manifest merge.
  */
 module.exports = function withFirebaseNotificationManifest(config) {
   return withAndroidManifest(config, (mod) => {
@@ -15,12 +17,31 @@ module.exports = function withFirebaseNotificationManifest(config) {
       throw new Error('Android manifest application element is missing.');
     }
 
-    const notificationColor = (application['meta-data'] || []).find(
+    application['meta-data'] = application['meta-data'] || [];
+
+    const notificationColor = application['meta-data'].find(
       (item) => item.$?.['android:name'] === FIREBASE_NOTIFICATION_COLOR
     );
 
     if (notificationColor) {
       notificationColor.$['tools:replace'] = 'android:resource';
+    }
+
+    let notificationIcon = application['meta-data'].find(
+      (item) => item.$?.['android:name'] === FIREBASE_NOTIFICATION_ICON
+    );
+
+    if (!notificationIcon) {
+      notificationIcon = {
+        $: {
+          'android:name': FIREBASE_NOTIFICATION_ICON,
+          'android:resource': '@drawable/notification_icon',
+        },
+      };
+      application['meta-data'].push(notificationIcon);
+    } else {
+      notificationIcon.$['android:resource'] = '@drawable/notification_icon';
+      notificationIcon.$['tools:replace'] = 'android:resource';
     }
 
     return mod;
